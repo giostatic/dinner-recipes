424271d54efca7ae49e8e3085845f75361675735
giostatic <116467840+giostatic@users.noreply.github.com>
2023-07-30 00:01:18 -1000

    adjusted nav bar css and for some reason arrow syntax doesnt work?

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let recipeLinks = ["https://giostatic.github.io/odin-recipes/recipes/ribs.html"]
 var button = document.getElementById("my-button");
 button.addEventListener("click", goSomewhere);
 
-const goSomewhere= () => {
+function goSomewhere() {
     let recipes = recipeLinks[Math.floor(Math.random()*recipeLinks.length)];
     window.location = recipes; //redirects the button
     document.getElementById("my-button").innerHTML = "YOU CLICKED ME!";
