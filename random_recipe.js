8a8e861095db9df75c29a871d3c779512e72f64f
James Lau <jklau88@hawaii.edu>
2023-07-29 22:49:06 -1000

    Update random_recipe.js
    Changed goSomewhere function. Button will also change text when clicked.

--- random_recipe.js
+++ random_recipe.js
@@ -1,6 +1,12 @@
 let recipeLinks = ["<a href='recipes/ribs.html'>Slow cooked ribs</a>"];
 
-let goSomewhere= (recipes) => {
+// let goSomewhere= (recipes) => {
+//     let recipes = recipeLinks[Math.floor(Math.random()*recipeLinks.length)];
+//     window.location = recipes; //redirects the button
+// };
+
+function goSomewhere {
     let recipes = recipeLinks[Math.floor(Math.random()*recipeLinks.length)];
     window.location = recipes; //redirects the button
-};
+    document.getElementById("my-button").innerHTML = "YOU CLICKED ME!";
+}
